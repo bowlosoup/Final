cdeaa973d92b68872a750ed4ade0af4821143156
bowlosoup <72242375+bowlosoup@users.noreply.github.com>
2020-10-16 10:28:03 -0600

    Update tripTester.java

--- finalProject/tripTester.java
+++ finalProject/tripTester.java
@@ -3,8 +3,15 @@ package finalProject;
 import java.util.Scanner;
 
 public class tripTester {
+	
 	public static void main(String args[])
 	{
+		int careff[] = {0,10,20,30};
+		int ow = careff[0];
+		int bmw = careff[1];
+		int lexus = careff[2];
+		int toyota = careff[3];
+		
 		tripCalculator calculate = new tripCalculator();
 		Scanner scanner = new Scanner(System.in);
 		
@@ -12,12 +19,18 @@ public class tripTester {
 		double cost = scanner.nextDouble();
 		System.out.print("Starting fuel in litres: ");
 		double gas = scanner.nextDouble();
-		System.out.print("Your car's fuel effeciency: ");
-		double fueleff = scanner.nextDouble();
+		System.out.print("Which car will you be using\n 1: bmw\n 2: lexus \n 3: toyota \n 1");
+		int fueleff = scanner.nextInt();
+	
 		System.out.print("How far are you going in kms?: ");
 		double distance = scanner.nextDouble();
-		System.out.println(calculate.fuelcost(cost));
+		System.out.println(calculate.totalfuel());
 		// distance, starting fuel, fuel efficiency
+		
+		switch(careff[fueleff])
+		{
+		
+		}
 	}
 
 }
